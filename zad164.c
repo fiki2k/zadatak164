775fa6c32be7176e77ddac551ee9b4387135ab09
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-05-24 10:55:55 +0200

    Add files via upload

---
+++ zad164.c
@@ -0,0 +1,30 @@
+// Program treba naci duljinu polja pomocu pointera.
+
+#include<stdio.h>
+#include<conio.h>
+
+int string_ln(char*);
+
+int main()
+{
+   char str[30];
+   int duljina;
+
+   printf("Unesi neki string: ");
+   scanf("%s",str);
+
+   duljina = string_duljina(str);
+   printf("\nDuljina danog stringa [ %s ] je : %d\n", str, duljina);
+
+  return 0;
+}
+
+int string_duljina(char*p) /* p=&str[0] */
+{
+   int brojac = 0;
+   while (*p != '\0') {
+      brojac++;
+      p++;
+   }
+   return brojac;
+}
